37959631fbe9510c6e6ee1205624454b8cc5ac89
Swapnilkk7753 <swapnilkesharwani3206@gmail.com>
2024-10-11 07:38:07 +0530

    Update Linkedlist.c

--- Linkedlist.c
+++ Linkedlist.c
@@ -1,50 +1,57 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<conio.h>
 #define NULL 0
-typedef struct node  {
-int data;
-struct node *next;
+//node
+typedef struct node
+{
+  int data;
+  struct node *next;
 }node;
+//creare linked list
 node * create(node *head)
 {
-    node p,*nhead;
-    int k;
-    nhead=head;
-    printf("enter the value for inserting\n");
-    scanf("%d",&k);
-    if(head==NULL)
+ node *nhead;
+ int i,k;
+ for(i=0;i<4;i++)
+ {
+  printf("enter the value for inserting\n");
+  scanf("%d",&k);
+  if(head==NULL)
     {
-      head=&p;
-      p.data=k;
-      p.next=NULL;
-    }
-    else{
-        while(nhead->next!=NULL)
-        {
-        nhead=nhead->next;
-         }
-        p.data=k;
-        nhead->next=&p;
-        p.next=NULL;
-          }
-    return head;
-}
+      head=(node *)malloc(1*sizeof(node));
+      head->data=k;
+      head->next=NULL;
+      nhead=head;
+    }//if ends
+  else
+    {
+     while(nhead->next!=NULL)
+     nhead=nhead->next;
+     nhead->next=(node *)malloc(1*sizeof(node));
+     nhead=nhead->next;
+     nhead->data=k;
+     nhead->next=NULL;
+    } //else ends
+  }//loop ends
+ return head;
+}//function ends
 void display(node *head)
 {
-    node *p=head;
-  printf("he\n");
-    while(p!=NULL)
+   printf("\nthe inserted element is= ");
+    while(head!=NULL)
     {
-        printf("%d\n",p->data);
-        p=p->next;
-    }
-}
+	printf("%d->",head->data);
+	head=head->next;
+    }//loop ends
+}//function ends
 int main()
 {
     node *head=NULL;
     int i;
-    for(i=0;i<3;i++)
+    clrscr();
     head=create(head);
     display(head);
+     getch();
     return 0;
-}
+}//main ends
